07bdec1b35f9d900aa4175348568acf42b230967
Harun Doğdu <harundogdu06@gmail.com>
2022-02-04 00:48:44 +0300

    chakra ui added to client

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { colors } from 'utils/theme';
+
+const theme = extendTheme({ colors })
 
 ReactDOM.render(
-    <App />,
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>,
   document.getElementById('root')
 );
