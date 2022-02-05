c6fc210aa5a96a7244a90ce4defe8af356b411b2
Harun Doğdu <harundogdu06@gmail.com>
2022-02-06 01:31:15 +0300

    redux-toolkit middleware serializabled

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,10 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { postApi } from 'services/postsApi'
+import { configureStore } from "@reduxjs/toolkit";
+import { postApi } from "services/postsApi";
+import authReducer from "features/auth/authSlice";
 
 export const store = configureStore({
     reducer: {
+        auth: authReducer,
         [postApi.reducerPath]: postApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(postApi.middleware),
-})
+        getDefaultMiddleware({
+            serializableCheck: false
+        }).concat(postApi.middleware),
+});
