44e864dad7fba6e25f0463dfbc36f64ce3b0ffd0
Harun Doğdu <harundogdu06@gmail.com>
2022-02-06 01:30:49 +0300

    custom toastify func created

---
+++ client/src/utils/flashMessages.js
@@ -0,0 +1,79 @@
+import { ToastContainer, toast } from 'react-toastify';
+
+export const ToastEmit = (type, message) => {
+    switch (type) {
+        case 'success':
+            toast.success(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            break;
+        case 'error':
+            toast.error(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            break;
+
+        case 'warning':
+            toast.warning(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            break;
+
+        case 'info':
+            toast.info(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            break;
+        default:
+            toast(message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+            break;
+    }
+}
+
+export const Toast = () => {
+    return (
+        <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
+    )
+}
