ca41b27721c6fc5fb0131822ac22143044464115
Harun Doğdu <harundogdu06@gmail.com>
2022-02-06 01:30:28 +0300

    react-roter-dom and react-toastify used in client

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { colors } from 'utils/theme';
 import { store } from 'features/store';
 import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 
 const theme = extendTheme({ colors })
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </ChakraProvider>,
   document.getElementById('root')
 );
